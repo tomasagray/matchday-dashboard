53dfa8a0aa5d7511a2e8c941b6c6532a0f3c829b
Tomás Gray <tomas.a.gray@gmail.com>
2022-08-09 15:53:20 -0700

    Remove 'preferences' (redundant)

--- src/app/App.js
+++ src/app/App.js
@@ -1,25 +1,19 @@
 import '../style/App.css';
 import '../style/main.scss';
-import React, {useContext} from "react";
+import React from "react";
 import {HeaderNav} from '../layout/HeaderNav';
 import {SideNav} from "../layout/SideNav";
 import {ContentStage} from "../layout/ContentStage";
-import {Preferences} from "./preferences";
-
 
 export default function App() {
 
-    const prefs = useContext(Preferences)
-
     return (
-        <Preferences.Provider value={prefs}>
-            <div className="App">
-                <HeaderNav/>
-                <div className="Scroll-wrapper">
-                    <SideNav/>
-                    <ContentStage/>
-                </div>
+        <div className="App">
+            <HeaderNav/>
+            <div className="Scroll-wrapper">
+                <SideNav/>
+                <ContentStage/>
             </div>
-        </Preferences.Provider>
+        </div>
     )
 }
