a8d7fd997973599372c8c05a7af154ba612c6752
Tom?s Gray <tomas.a.gray@gmail.com>
2023-10-08 21:29:40 +0000

    Add installation script

---
+++ install.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+if [ $# -eq 0 ] || [ "$1" == "-h" ]; then
+  echo "Usage:  install.sh <installation_path>"
+  exit 1
+fi
+
+INSTALL_DIR=$1
+echo "Installing Matchday Dashboard to: ${INSTALL_DIR}..."
+# ensure installation directory exists
+if [[ ! (-d ${INSTALL_DIR}) ]]; then
+  echo "Directory: ${INSTALL_DIR} does not exist; quitting..."
+  exit 1
+fi
+
+echo "This will PERMANENTLY delete the previous installation."
+read -rp "Are you SURE you want to proceed? [y/N]: " proceed
+
+if [[ "yes" == *${proceed,,}* ]]; then
+  echo "Cleaning up previous build(s)..."
+  rm ./build/ -rf
+  echo "Building..."
+  npm run build
+  rm "${INSTALL_DIR:?}/*" -rf
+  cp -r ./build/* "$INSTALL_DIR"
+  echo "Installation completed successfully."
+else
+  echo "Quitting..."
+fi
